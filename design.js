b182f1556173e3605dcd0bd1461c3e4f230e5a1a
parkertimmins <parkertimmins@gmail.com>
2019-03-23 10:44:57 -0500

    added user controls for speed, square size, and color

--- design.js
+++ design.js
@@ -1,75 +1,75 @@
 
-function zip(a, b) {
-    const res = [];
-    for (var i = 0; i < a.length; i++) {
-        res.push([a[i], b[i]]);
+function add_stars() {
+    // skip some frames
+    if (Math.random() > 0.2) {
+        return;
     }
-    return res;
+
+    let star_count = 5;
+    const canvas = document.getElementById("canvas-refreshing");
+    var ctx = canvas.getContext("2d");
+    while (star_count-- > 0) {
+        const right = Math.random() * canvas.width;
+        const down = Math.random() * canvas.height;
+        const size = Math.random() > 0.5 ? 2 : 1;
+        ctx.beginPath(); //Start path
+        ctx.fillStyle = randBoolean() ? "snow" : "#2ae4fc";
+        ctx.arc(right, down, size, 0, Math.PI * 2, true); // Draw a point using the arc function of the canvas with a point structure.
+        ctx.fill();
+        ctx.closePath();
+    }
 }
+
+function randBoolean() {
+    return Math.random() > 0.5;
+}    
 
 function clear_canvas() {
     const canvas = document.getElementById("canvas-refreshing");
     const context = canvas.getContext('2d');
     context.clearRect(0, 0, canvas.width, canvas.height);
-    //context.fillRect(0,0, canvas.width, canvas.height);
 }
 
-function draw_square(right, down, square_size) {
+function draw_square(right, down, square_size, color) {
     const canvas = document.getElementById("canvas-refreshing");
     var ctx = canvas.getContext("2d");
     ctx.beginPath();
-    ctx.fillStyle = "blue";
+    ctx.fillStyle = color;
     ctx.fillRect(right, down, square_size, square_size);
     ctx.stroke();
     ctx.closePath();
 }
 
-function range(n) {
-    return Array(n).fill().map((_, i) => i);
-}
-
 let startTime = null;
-
 let vertical = true;
 
 function step(timestamp) {
+    if (!startTime) {
+        startTime = timestamp;
+    }
+    
     clear_canvas();
-
-    if (!startTime) startTime = timestamp;
-
-    const timeAnimateOneSide = 2000; // 3 seconds
-
+    add_stars();
+    
+    const num_squares = parseInt(document.getElementById("num-squares").value);
+    const timeAnimateOneSide = parseFloat(document.getElementById("animation-speed").value); 
     const progress = (timestamp - startTime) / timeAnimateOneSide;
-
-    const main_square_len = 800;
-
-    const num_squares = 20;
+    const main_square_len = document.getElementById("canvas-refreshing").width;
     const square_side_len = main_square_len / num_squares;
    
-    const ids = range(num_squares);
+    for (let id = 0; id < num_squares; id++) {
+        const start = { right: id * square_side_len, down: id * square_side_len };
+        const end = { right: id * square_side_len, down: main_square_len - square_side_len - id * square_side_len };
+        let now = { right: start.right, down: (end.down - start.down) * progress + start.down };
+        
+        if (!vertical) {
+            now = { down: now.right, right: main_square_len - now.down - square_side_len };
+        }
 
-    // give coords of UL corner where UL is 0,0
-    const startLocation = id => ({ right: id * square_side_len, down: id * square_side_len });
-    const endLocation = id => ({ right: id * square_side_len, down: main_square_len - square_side_len - id * square_side_len });
-
-    const startLocations = ids.map(startLocation);
-    const endLocations = ids.map(endLocation);
-    const startEnds = zip(startLocations, endLocations); 
-
-    let locationAtTime = startEnds.map((startEnd) => {
-        const start = startEnd[0];
-        const end = startEnd[1];
-        return { right: start.right, down: (end.down - start.down) * progress + start.down };
-    });
-
-    console.log(locationAtTime.length);
-
-    if (!vertical) {
-        locationAtTime = locationAtTime.map(square => ({ down: square.right, right: main_square_len - square.down - square_side_len }));
+        const color = document.getElementById("color-input").value;
+        draw_square(now.right, now.down, square_side_len, color);
     }
-
-    locationAtTime.forEach(square => draw_square(square.right, square.down, square_side_len));
-
+    
     if (progress > 1) {
         startTime = timestamp;
         vertical = !vertical
@@ -77,12 +77,7 @@ function step(timestamp) {
     window.requestAnimationFrame(step);
 }
 
-function startAnimation() {
-    window.requestAnimationFrame(step);
-}
-
-// on load code  ////////////////////////////////////////////////
 window.onload = function () {
-    startAnimation();	
+    window.requestAnimationFrame(step);
 }
 
